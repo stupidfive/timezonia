98de36ca5497789a2850e1d649780b98ae1458d5
GeorgeWu <stupidfive@gmail.com>
2021-02-25 15:02:31 +0800

    Remove spaces in names

--- __tests__/index.specs.js
+++ __tests__/index.specs.js
@@ -7,19 +7,19 @@ describe('Timezones', () => {
 
   describe('get UTC timezone', () => {
     it('should get "UTC +8"', () => {
-      expect(utcOf(-480)).toBe('UTC +8')
+      expect(utcOf(-480)).toBe('UTC+8')
     });
     it('should get "UTC +0"', () => {
-      expect(utcOf(0)).toBe('UTC +0')
+      expect(utcOf(0)).toBe('UTC+0')
     });
     it('should get "UTC -0', () => {
-      expect(utcOf(60)).toBe('UTC -1')
+      expect(utcOf(60)).toBe('UTC-1')
     })
     it('should get "UTC -3:30', () => {
-      expect(utcOf(210)).toBe('UTC -3:30')
+      expect(utcOf(210)).toBe('UTC-3:30')
     })
     it('should get "UTC +8:45', () => {
-      expect(utcOf(-525)).toBe('UTC +8:45')
+      expect(utcOf(-525)).toBe('UTC+8:45')
     })
     it('should print correct UTC', () => {
       console.log(currentUtc());
